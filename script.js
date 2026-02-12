fab51041b3a655e680238fca7cdae8d3bd5e951d
damascenoa49-sys <damascenoa49@gmail.com>
2026-02-12 16:37:00 -0300

    Refactor map layer loading and styles
    Removed old code for loading map layers and added new implementation for loading municipalities and quilombos data with updated styles.

--- script.js
+++ script.js
@@ -1,151 +1,55 @@
-console.log("Inicializando WebGIS...");
-
-// =============================
-// CAMADAS BASE
-// =============================
-
-var osm = L.tileLayer(
-  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  { attribution: "&copy; OpenStreetMap contributors" }
-);
-
-var esriSat = L.tileLayer(
-  "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-  { attribution: "Tiles &copy; Esri" }
-);
-
-var googleSat = L.tileLayer(
-  "http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
-  {
-    maxZoom: 20,
-    subdomains: ["mt0", "mt1", "mt2", "mt3"],
-    attribution: "© Google"
+// ===============================
+// LOADER
+// ===============================
+window.onload = function () {
+  var loader = document.getElementById("loader");
+  if (loader) {
+    loader.style.display = "none";
   }
-);
-
-// =============================
-// MAPA
-// =============================
-
-var map = L.map("map", {
-  center: [-5.5, -45],
-  zoom: 7,
-  layers: [esriSat],
-  preferCanvas: true
-});
-
-// =============================
-// CONTROLE
-// =============================
-
-var baseMaps = {
-  "OpenStreetMap": osm,
-  "Satélite Esri": esriSat,
-  "Satélite Google": googleSat
 };
 
-var overlayMaps = {};
-var controlLayers = L.control.layers(baseMaps, overlayMaps).addTo(map);
+// ===============================
+// MAPA
+// ===============================
+var map = L.map("map").setView([-5.0, -45.0], 6);
 
-// =============================
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "&copy; OpenStreetMap contributors"
+}).addTo(map);
+
+// ===============================
 // VARIÁVEIS GLOBAIS
-// =============================
-
+// ===============================
 var municipiosLayer;
 var quilombosLayer;
-var municipiosData;
-var quilombosData;
 
-// =============================
+// ===============================
 // CARREGAR MUNICÍPIOS
-// =============================
-
-fetch("./municipios_ma.json")
-  .then(res => res.json())
-  .then(function(data) {
-
-    municipiosData = data;
+// ===============================
+fetch("dados/municipios_ma.json")
+  .then(response => response.json())
+  .then(data => {
 
     municipiosLayer = L.geoJSON(data, {
       style: {
-        color: "#000",
+        color: "#555",
         weight: 1,
-        fillOpacity: 0.15
+        fillOpacity: 0.2
       }
     }).addTo(map);
 
-    overlayMaps["Municípios"] = municipiosLayer;
-    controlLayers.addOverlay(municipiosLayer, "Municípios");
   });
 
-// =============================
+// ===============================
 // CARREGAR QUILOMBOS
-// =============================
-
-fetch("./quilombos_ma.json")
-  .then(res => res.json())
-  .then(function(data) {
-
-    quilombosData = data;
+// ===============================
+fetch("dados/quilombos_ma.json")
+  .then(response => response.json())
+  .then(data => {
 
     quilombosLayer = L.geoJSON(data, {
-      pointToLayer: function(feature, latlng) {
-        return L.circleMarker(latlng, {
-          radius: 6,
-          fillColor: "#0077ff",
-          color: "#000",
-          weight: 1,
-          fillOpacity: 0.9
-        });
-      },
-      onEachFeature: function(feature, layer) {
 
-        var props = feature.properties || {};
-
-        var popupContent =
-          "<b>Nome:</b> " + (props.NOM_TQ || "") + "<br>" +
-          "<b>Código:</b> " + (props.COD_TQ || "") + "<br>" +
-          "<b>Status:</b> " + (props.STATUS || "");
-
-        layer.bindPopup(popupContent);
-      }
-    }).addTo(map);
-
-    overlayMaps["Quilombos"] = quilombosLayer;
-    controlLayers.addOverlay(quilombosLayer, "Quilombos");
-  });
-
-// =============================
-// FUNÇÃO DE FILTRO
-// =============================
-
-function filtrarMunicipio() {
-
-  var nomeBusca = document.getElementById("buscaMunicipio").value.toUpperCase();
-
-  if (!nomeBusca) return;
-
-  // Encontrar município
-  var municipioEncontrado = municipiosData.features.find(function(f) {
-    return f.properties.NOME_MUN.toUpperCase().includes(nomeBusca);
-  });
-
-  if (!municipioEncontrado) {
-    alert("Município não encontrado.");
-    return;
-  }
-
-  // Zoom no município
-  var layerMunicipio = L.geoJSON(municipioEncontrado);
-  map.fitBounds(layerMunicipio.getBounds());
-
-  // Filtrar quilombos
-  quilombosLayer.clearLayers();
-
-  var filtrados = quilombosData.features.filter(function(f) {
-    return f.properties.MUNICIPIO &&
-           f.properties.MUNICIPIO.toUpperCase() === municipioEncontrado.properties.NOME_MUN.toUpperCase();
-  });
-
-  quilombosLayer.addData(filtrados);
-}
+      style: {
+        color: "#003366",
+        weight: 1,
+        fillColor: "#0077ff",
